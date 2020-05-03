9fb8162510aec18bae82df00220ff9de7169af6d
Dan Booth <DanBoothDev@gmail.com>
2020-05-03 14:50:46 -0300

    Added body parser to make handling requests nicer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const app = require('express')();
 const routes = require('./routes');
+const bodyParser = require("body-parser");
+
+// setup environment vars
 require('dotenv').config()
+
+// parse as json
+app.use(bodyParser.json());
 
 // setup routes
 app.use('/', routes);
